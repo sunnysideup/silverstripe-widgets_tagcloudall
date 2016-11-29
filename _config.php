cfbc89f81003f3b5fabe59bbe8d98c8f59f367b6
sunnysideup <sunnysideupdevs@gmail.com>
2016-11-29 14:04:22 +1300

    PATCH: module clean-up

--- _config.php
+++ _config.php
@@ -9,4 +9,3 @@
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START widgets_tagcloudall MODULE ----------------===================
 //===================---------------- END widgets_tagcloudall MODULE ----------------===================
-
